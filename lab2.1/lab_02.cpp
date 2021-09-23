f95c8c50ddaea09d41d97c296bb1846439a3a7f5
Andriy Kobetiak <andriykobetiak@gmail.com>
2021-09-23 07:39:34 +0300

    Lab2.1 release

---
+++ lab2.1/lab_02.cpp
@@ -0,0 +1,28 @@
+﻿// lab_02.cpp
+// Кобетяк Андрій
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 12
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	const double Pi = 4 * atan(1.); // число пі
+
+	double x, z1, z2; //оголошення змінних
+
+	cout << "x = "; cin >> x; cout << endl;
+
+	z1 = (sin(4 * x) / (1 + cos(4 * x))) *
+		(cos(2 * x) / (1 + cos(2 * x))); // Розрахунок першої формули
+
+	z2 = cos(((3. / 2) * Pi) - x) /
+		sin(((3. / 2) * Pi) - x); // Розрахунок другої формули
+	
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
