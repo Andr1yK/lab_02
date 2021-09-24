471b4e1770e2628c2951da24d89559f0ace10e35
Andriy Kobetiak <andriykobetiak@gmail.com>
2021-09-24 11:09:44 +0300

    add z3 variable

--- lab2.1/lab_02.cpp
+++ lab2.1/lab_02.cpp
@@ -11,7 +11,7 @@ int main()
 {
 	const double Pi = 4 * atan(1.); // число пі
 
-	double x, z1, z2; //оголошення змінних
+	double x, z1, z2, z3; //оголошення змінних
 
 	cout << "x = "; cin >> x; cout << endl;
 
@@ -21,8 +21,11 @@ int main()
 	z2 = cos(((3. / 2) * Pi) - x) /
 		sin(((3. / 2) * Pi) - x); // Розрахунок другої формули
 	
+	z3 = z1 + z2;
+
 	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
+	cout << "z3 = " << z3 << endl;
 	cin.get();
 	return 0;
 }
